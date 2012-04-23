0ef82f8f8a6da29d116ede16791b1d19ed6852ac
Pradeep Banavara <pradeep@fastr.in>
2012-04-23 09:44:54 +0530

    Initial checkin of the Android client

---
+++ Android_client/src/in/company/taxiguide/BackgroundServiceStarter.java
@@ -0,0 +1,16 @@
+package in.company.taxiguide;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BackgroundServiceStarter extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		//DBHelper helper = new DBHelper(getApplicationContext());	
+
+	}
+
+}
